aeb033791b90b424632ed4ead48d51f4b96ee56e
Dave Avis <charles.avis@springbranchisd.com>
2021-03-30 13:55:14 +0000

    initial setup

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Main
 {
-  public static void main(String[] args)
-  {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) throws IOException
+    {
+        Scanner file = new Scanner(new File("maze.dat"));
+            while(file.hasNext())
+            {
+                int size = file.nextInt();
+                file.nextLine();
+                Maze test = new Maze(size, file.nextLine());
+                test.hasExitPath(0,0);
+                System.out.println(test);
+            }
+    }
 }
---
+++ Maze.java
@@ -0,0 +1,27 @@
+public class Maze
+{
+   private int[][] maze;
+   private boolean exitFound;
+
+	public Maze(int size, String line)
+	{
+        // set exitFound to false to start
+        // instantiate a new 2 dimensional int array of the appropriate size
+        // populate the maze array using nested for loops
+
+	}
+
+    // You can use this as a boolean return method, or you can change 
+    // it to a void return method and set exitFound inside it.  
+    // You may choose.
+	public boolean hasExitPath(int r, int c)
+	{
+		return false;
+	}
+
+	public String toString()
+	{
+		String output="";
+		return output;
+	}
+}
